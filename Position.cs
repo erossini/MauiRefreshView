af5e1b22549801f6177565d56b6637cf25fa07a4
Enrico Rossini <erossini@users.noreply.github.com>
2025-09-10 22:51:49 +0100

    Refactor Position.cs namespace and formatting

--- Position.cs
+++ Position.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LanguageInUse.Components.CustomRefresh
+namespace <yProject.Components.CustomRefresh
 {
     public enum Position
     {
